34258d5296e2e382b4d86146049afd335a35afb7
Michael Woolnough <michael.woolnough@gmail.com>
2014-09-03 17:53:05 +0100

    Added helper methods

--- mw/library/BlockManipulator.java
+++ mw/library/BlockManipulator.java
@@ -4,8 +4,8 @@ import net.minecraft.block.Block;
 
 public class BlockManipulator {
 	
-	private static IBlockManipulator[] manipulatorList = new IBlockManipulator[4096];
-	private static DefaultManipulators.Default defaultManipulator = new DefaultManipulators.Default();
+	private static final IBlockManipulator[] manipulatorList = new IBlockManipulator[4096];
+	private static final IBlockManipulator defaultManipulator = new DefaultManipulators.Default();
 	
 	public static boolean registerManipulator(Block block, IBlockManipulator bm) {
 		int blockId = block.blockID;
@@ -21,6 +21,26 @@ public class BlockManipulator {
 			return manipulatorList[blockId];
 		}
 		return defaultManipulator;
+	}
+	
+	public static Blocks rotate90(Blocks block) {
+		return getManipulator(block.blockId).rotate90(block);
+	}
+	
+	public static Blocks rotate180(Blocks block) {
+		return getManipulator(block.blockId).rotate180(block);
+	}
+	
+	public static Blocks rotate270(Blocks block) {
+		return getManipulator(block.blockId).rotate270(block);
+	}
+	
+	public static Blocks mirrorX(Blocks block) {
+		return getManipulator(block.blockId).mirrorX(block);
+	}
+	
+	public static Blocks mirrorZ(Blocks block) {
+		return getManipulator(block.blockId).mirrorZ(block);
 	}
 	
 	static {
